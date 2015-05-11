8da2159f05e5ed857dd2fae64fcd3a111db5bda2
ReenExe <ipourri@gmail.com>
2015-05-11 22:48:43 +0300

    first test

--- python/flask/TicTacTest.py
+++ python/flask/TicTacTest.py
@@ -1,8 +1,18 @@
 import unittest
+from tictac import TicTacGame
 
 class TicTacTest(unittest.TestCase):
 
     def test(self):
         self.assertTrue(True)
+
+    def testGame(self):
+        players = {
+            'first':  TicTacGame(TicTacGame.CHOOSES[0]),
+            'second': TicTacGame(TicTacGame.CHOOSES[1]),
+        }
+
+        self.assertEqual(players['first'].choose, 'X')
+        self.assertEqual(players['second'].choose, '0')
 
 unittest.main()
---
+++ python/flask/tictac.py
@@ -0,0 +1,4 @@
+class TicTacGame(object):
+    CHOOSES = ('X', '0')
+    def __init__(self, choose):
+        self.choose = choose
