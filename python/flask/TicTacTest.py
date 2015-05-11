34e418021779d6b9fccfde0db0eb3497b7cebed9
ReenExe <ipourri@gmail.com>
2015-05-11 23:02:10 +0300

    first step

--- python/flask/TicTacTest.py
+++ python/flask/TicTacTest.py
@@ -1,5 +1,5 @@
 import unittest
-from tictac import TicTacGame
+from tictac import TicTacGame, TicTacPriority
 
 class TicTacTest(unittest.TestCase):
 
@@ -14,5 +14,10 @@ class TicTacTest(unittest.TestCase):
 
         self.assertEqual(players['first'].choose, 'X')
         self.assertEqual(players['second'].choose, '0')
+
+    def testStep(self):
+        player = TicTacGame('X')
+        step = player.start()
+        self.assertTrue(step in TicTacPriority.MAP)
 
 unittest.main()
--- python/flask/tictac.py
+++ python/flask/tictac.py
@@ -1,4 +1,17 @@
+
+class TicTacPriority(object):
+    # 0 1 2
+    # 3 4 5
+    # 6 7 8
+    MAP = (4, 1, 2, 5, 8, 7, 6, 3, 0)
+
 class TicTacGame(object):
     CHOOSES = ('X', '0')
     def __init__(self, choose):
         self.choose = choose
+        self.history = []
+    def start(self):
+        if (self.choose == 'X' and len(self.history) == 0):
+            step = TicTacPriority.MAP[0]
+            self.history.append(step)
+            return step
