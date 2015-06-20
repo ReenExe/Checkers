545afbdecd9ab80bd7d513f3f8946e49e9207172
ReenExe <ipourri@gmail.com>
2015-06-21 00:10:28 +0300

    mono C# progress

--- mono/TicTacToe/TicTacToe/Choice.cs
+++ mono/TicTacToe/TicTacToe/Choice.cs
@@ -4,8 +4,19 @@ namespace TicTacToe
 {
 	public class Choice
 	{
-		public Choice ()
+		public const char CROSS = 'X';
+		public const char ZERO = '0';
+
+		private char choice;
+
+		public Choice(char choice)
 		{
+			this.choice = choice;
+		}
+
+		public bool begginer()
+		{
+			return this.choice == Choice.CROSS;
 		}
 	}
 }
--- mono/TicTacToe/TicTacToe/TicTacToePlayerTest.cs
+++ mono/TicTacToe/TicTacToe/TicTacToePlayerTest.cs
@@ -7,9 +7,21 @@ namespace TicTacToe
 	public class TicTacToePlayerTest
 	{
 		[Test()]
+		public void Choice()
+		{
+			Choice cross = new Choice(TicTacToe.Choice.CROSS);
+
+			Assert.IsTrue(cross.begginer());
+
+			Choice zero = new TicTacToe.Choice(TicTacToe.Choice.ZERO);
+
+			Assert.IsFalse (zero.begginer());
+		}
+
+		[Test()]
 		public void First()
 		{
-			Player player = new Player(new Choice());
+			Player player = new Player(new TicTacToe.Choice(TicTacToe.Choice.CROSS));
 
 			Assert.IsTrue(player.answer());
 		}
